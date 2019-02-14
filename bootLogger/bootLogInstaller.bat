f06f1cc26183c8f14e57bf6ac18e4cc60ba8f4c1
CoderTypist <39784989+CoderTypist@users.noreply.github.com>
2019-02-14 16:04:12 -0600

    Create bootLogInstaller.bat
    My initial commit for bootLogInstaller.bat

---
+++ bootLogger/bootLogInstaller.bat
@@ -0,0 +1,20 @@
+@echo off
+
+rem creates the folder in which to put bootLog.txt
+if not exist "C:\ProgramData\Programs" (
+    mkdir "C:\ProgramData\Programs"
+    attrib +h "C:\ProgramData\Programs"
+)
+
+rem creates bootLog.txt
+if not exist "C:\ProgramData\Programs\bootLog.txt" (
+    type nul > "C:\ProgramData\Programs\bootLog.txt"
+    attrib +h "C:\ProgramData\Programs\bootLog.txt"
+)
+
+rem creates bootLogger.bat
+if not exist "C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\bootLogger.bat" (
+    type nul > boolLogger.bat
+    attrib +h "C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\bootLogger.bat"
+    echo echo %date% %time% %username% ^>^> "C:\ProgramData\Programs\bootLog.txt" >"C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\bootLogger.bat"
+)
